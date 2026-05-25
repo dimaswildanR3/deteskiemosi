a434c787869de3bbb67f0b060a8c038bac713dcc
dimaswildan28 <dimaswildan1986@gmail.com>
2026-05-25 08:54:45 +0700

    test

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -381,8 +381,8 @@
 <script src="{{ asset('vendor/bootstrap/js/bootstrap.min.js') }}"></script>
 <script src="{{ asset('vendor/jquery-easing/jquery.easing.min.js') }}"></script>
 <script src="https://deteksiemosi.com/js/sb-admin-2.min.js"></script>
-<!-- <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script> -->
-    <!-- <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script> -->
+
+<script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 
     @stack('js')
 </body>
