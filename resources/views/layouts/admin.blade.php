9262bfdf935ae702e098ad6438ad67204417419b
dimaswildan28 <dimaswildan1986@gmail.com>
2026-05-25 08:57:22 +0700

    git push

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -377,11 +377,13 @@
 </div>
 
 <!-- Scripts -->
-<script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
-<script src="{{ asset('vendor/bootstrap/js/bootstrap.min.js') }}"></script>
-<script src="{{ asset('vendor/jquery-easing/jquery.easing.min.js') }}"></script>
-<script src="https://deteksiemosi.com/js/sb-admin-2.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/jquery-easing@1.4.1/jquery.easing.min.js"></script>
+
+<script src="https://deteksiemosi.com/js/sb-admin-2.min.js"></script>
 <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 
     @stack('js')
